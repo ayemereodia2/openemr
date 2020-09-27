06a7ffbf9a59a0333e2ee0d43a36f86cc1dd8754
Ayemere Odia <odiadavid2@gmail.com>
2020-09-27 13:28:04 +0100

    Update sqlconf.php

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -8,8 +8,8 @@ $disable_utf8_flag = false;
 
 $host   = 'k00nzj7gqn4v73x1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $port   = '3306';
-$login  = 'openemr';
-$pass   = 'Administrator1234';
+$login  = 'esjs58unjy4kxr5l';
+$pass   = 'zf25y60jfliag9mr';
 $dbase  = 'primary_app_db';
 
 $sqlconf = array();
